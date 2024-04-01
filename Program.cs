9319100f8a907bb735744864999f4cefa44046b0
[Arjav Patel] <arjavjp@gmail.com>
2024-04-01 14:40:41 -0400

    Second commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Week10Lab.Models.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        using (var dbContext = new MMABooksContext())
+        {
+            
+            var customers = dbContext.Customers.ToList();
+
+            
+            Console.WriteLine("Customer Names:");
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+        }
+    }
+}
